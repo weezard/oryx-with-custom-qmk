76ad84d1a850b8de61c8a4444b8d5ef9b035a35e
Francisco Hernandez <dev@weezards.com>
2025-02-18 18:47:58 +0100

    added repeat key

--- BqYQd/keymap.c
+++ BqYQd/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LT(3,KC_ESCAPE),KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           LT(5,KC_GRAVE), 
     MT(MOD_LCTL, KC_F13),KC_A,           KC_S,           LT(1,KC_D),     KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        MT(MOD_RCTL, KC_QUOTE),
     OSM(MOD_LSFT),  KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       OSM(MOD_RSFT),  
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_F17,         OSM(MOD_LALT),  OSM(MOD_LGUI),  KC_SPACE,                                       KC_F17,         MT(MOD_RGUI, KC_ENTER),MT(MOD_RALT, KC_LBRC),KC_RBRC,        KC_TRANSPARENT, KC_TRANSPARENT, 
+    QK_REPEAT_KEY, KC_TRANSPARENT, KC_F17,         OSM(MOD_LALT),  OSM(MOD_LGUI),  KC_SPACE,                                       KC_F17,         MT(MOD_RGUI, KC_ENTER),MT(MOD_RALT, KC_LBRC),KC_RBRC,        KC_TRANSPARENT, QK_REPEAT_KEY, 
                                                     LT(7,KC_TAB),   ALL_T(KC_F18),                                  ALL_T(KC_F19),  LT(4,KC_BSPC)
   ),
   [1] = LAYOUT_voyager(
